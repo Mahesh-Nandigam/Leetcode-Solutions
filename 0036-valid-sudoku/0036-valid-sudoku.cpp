46bf58c92eb6c67cf212d8df6190d1d34984e1cb
MAHESH NANDIGAM <nandigammahesh595@gmail.com>
2026-09-25 00:07:46 +0530

    Time: 4 ms (37.92%), Space: 21.6 MB (92.29%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+bool isrow(vector<vector<char>>& board,int i,int num){
+int ct=0;
+for(int j=0;j<9;j++){
+    if(board[i][j]-'0'==num)
+    ct++;
+}
+if(ct>=2)
+return false;
+return true;
+}
+bool iscol(vector<vector<char>>& board,int j,int num){
+int ct=0;
+for(int i=0;i<9;i++){
+    if(board[i][j]-'0'==num)
+    ct++;
+}
+if(ct>=2)
+return false;
+return true;
+}
+bool ismat3(vector<vector<char>>&board,int i,int j,int num){
+int ct=0;
+int row=i/3;
+int col=j/3;
+row*=3;
+col*=3;
+for(int a=row;a<=row+2;a++){
+    for(int b=col;b<=col+2;b++){
+        if(board[a][b]-'0'==num)
+        ct++;
+    }
+}
+if(ct>=2)
+return false;
+return true;
+}
+    bool isValidSudoku(vector<vector<char>>& board) {
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(board[i][j]!='.'){
+            int num=board[i][j]-'0';
+            if(iscol(board,j,num) && isrow(board,i,num) && ismat3(board,i,j,num))
+            continue;
+            else
+                return false;
+            }
+        }
+        
+    }
+    return true;
+    }
+};
